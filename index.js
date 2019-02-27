80b6835c42f52bdf08449fecf33a26bc080165cb
CuriousJoshy <47984839+CuriousJoshy@users.noreply.github.com>
2019-02-26 22:32:41 -0800

    Installed uniqid and modified scripts

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var uniqid = require("uniqid");
+
+/* app.get('/', function(req, res){	
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+}); */
+
+// http.listen(3000, "192.168.1.10");
+
+http.listen(3000, function(){
+	console.log('listening on *:3000',uniqid());
+});
---
+++ src/chatroom.js
@@ -0,0 +1,4 @@
+var ChatRoom = function()
+{
+	
+};
